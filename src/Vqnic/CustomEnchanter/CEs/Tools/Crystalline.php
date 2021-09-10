6d05bc35f3ee80cd5320cc601af3d96aeeb6d132
VANIC <boyd@pocketplugins.dev>
2021-09-10 14:46:44 -0400

    Update Crystalline.php

--- src/Vqnic/CustomEnchanter/CEs/Tools/Crystalline.php
+++ src/Vqnic/CustomEnchanter/CEs/Tools/Crystalline.php
@@ -5,18 +5,40 @@ namespace Vqnic\CustomEnchanter\CEs\Tools;
 
 use Vqnic\CustomEnchanter\CEs\Tools\ToolCE;
 use Vqnic\CustomEnchanter\Main;
+use pocketmine\Player;
+use pocketmine\pocketmine\Block;
 
 class Crystalline extends ToolCE{
 
     private int $CElevel;
+    private Player $player;
+    private Block $block;
 
-    function __construct($l, $a, $v){
+    function __construct($l, $b, $p){
         $this->CElevel = $l;
+        $this->player = $p;
+        $this->block = $b;
     }
 
     public function trigger(){
         if(mt_rand(1, 100) <= 2){ // 2% chance
             //do a thing
+            switch($this->CElevel){
+                case 1:
+                    break;
+
+                case 2:
+
+                    break;
+
+                case 3:
+
+                    break;
+
+                case 4:
+
+                    break;
+            }
         }
     }
 }
