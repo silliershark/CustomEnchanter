dab61850a33ef96c6aca0d94227a77c8a1f896df
VANIC <boyd@pocketplugins.dev>
2021-09-10 14:56:31 -0400

    Update Main.php

--- src/Vqnic/CustomEnchanter/Main.php
+++ src/Vqnic/CustomEnchanter/Main.php
@@ -2,6 +2,7 @@
 
 namespace Vqnic\CustomEnchanter;
 
+use pocketmine\Server;
 use pocketmine\plugin\PluginBase;
 
 use Vqnic\CustomEnchanter\CEListener;
@@ -13,6 +14,11 @@ class Main extends PluginBase{
     public function onEnable(){
         self::$instance = $this;
         $this->getServer()->getPluginManager()->registerEvents(new CEListener($this), $this);
+        //basic "spoon" detector, inspired by this repo:
+        //https://github.com/falkirks/spoondetector
+        if(!in_array(Server::getInstance()->getName(), "PocketMine-MP")){
+            $this->getServer()->getLogger()->warning("Hey there! It appears as if you are using a Spoon. Just so you are aware, we do not offer support for those who are using this plugin on a Spoon. If you would like to recieve support for a bug, please use normal PocketMIne-MP.");
+        }
     }
 
     public static function getMain() : Main{
